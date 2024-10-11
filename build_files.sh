474522dacd9d4c27dca407820487a5d407e2df6d
surojitmondal <gobindomondal539@gmail.com>
2024-10-12 00:52:55 +0530

    minor changes

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,14 @@
 echo "BUILD START"
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+# python3.9 -m venv venv
+# source venv/bin/activate
 
-# activate the virtual environment
-source venv/bin/activate
+# /vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
 
-/vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
-
-# install all deps in the venv
 pip install -r requirements.txt
-
-# collect static files using the Python interpreter from venv
 python3.9 manage.py collectstatic --noinput
 
 echo "BUILD END"
 
-# [optional] Start the application here 
+
 # python manage.py runserver
