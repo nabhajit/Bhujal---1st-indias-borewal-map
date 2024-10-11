7b37141d339d062234637657677fde156eb8fdef
surojitmondal <gobindomondal539@gmail.com>
2024-10-12 00:58:52 +0530

    minor changes

--- build_files.sh
+++ build_files.sh
@@ -3,7 +3,7 @@ echo "BUILD START"
 python3.9 -m venv venv
 source venv/bin/activate
 
-# /vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
+/vercel/path0/venv/bin/python3.9 -m pip install --upgrade pip
 
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
